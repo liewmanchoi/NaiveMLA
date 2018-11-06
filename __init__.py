86e157255f775492bcf9855d787fe3676e205bb3
Liew Manchoi <liewmanchoi@gmail.com>
2018-11-06 21:06:37 +0800

    在根目录添加__init__.py文件
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# __author__ = "Liew Manchoi"
+# __copyright__ = "Copyright 2018, Trump Organization"
+# __email__ = "liewmanchoi@gmail.com"
+# __status__ = "experiment"
+# __time__ = 2018/11/6 9:32
+# __file__ = __init__.py
