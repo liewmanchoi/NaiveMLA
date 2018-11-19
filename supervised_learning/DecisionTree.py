8f8e5339d2c1de3bdbe8fe56cce70bccf5a3da17
Liew Manchoi <liewmanchoi@gmail.com>
2018-11-19 20:12:22 +0800

    fix: 修改DecisionTreeClassifier中的错误
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

--- supervised_learning/DecisionTree.py
+++ supervised_learning/DecisionTree.py
@@ -9,21 +9,25 @@
 import numpy as np
 import abc
 import math
+from utils.data import is_discrete_target
+from sklearn.metrics import accuracy_score
 
 
 class DecisionTreeNode(object):
     def __init__(self, feature_idx: int = None, cut_off_point: float = None, leaf_output_value=None,
-                 left_child: 'DecisionTreeNode' = None, right_child: 'DecisionTreeNode' = None):
+                 left_child: 'DecisionTreeNode' = None, right_child: 'DecisionTreeNode' = None,
+                 is_discrete: bool = None):
         self.feature_idx = feature_idx  # 本节点划分所依据的特征索引值
         self.cut_off_point = cut_off_point   # feature_idx处的特征值划分的阈值
         self.leaf_output_value = leaf_output_value  # 叶子节点（如果是叶子节点）的输出值
         self.left_child = left_child  # 左子节点
         self.right_child = right_child  # 右子节点
+        self.is_discrete = is_discrete
 
 
 class BaseDecisionTree(object):
     def __init__(self, max_depth: int = float("inf"), min_samples_split: int = 2,
-                 min_impurity_split: float = 1e-7):
+                 min_impurity_split: float = -float("inf")):
         self.root: 'DecisionTreeNode' = None
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
@@ -44,57 +48,60 @@ class BaseDecisionTree(object):
     def _generate_tree(self, X: np.ndarray, y: np.ndarray, depth: int = 0) -> 'DecisionTreeNode':
         n_samples, n_features = X.shape
 
+        if n_samples < self.min_samples_split or depth >= self.max_depth:
+            return DecisionTreeNode(leaf_output_value=self._leaf_value_func(y))
+
         min_impurity: float = float('inf')
+        best_feature_is_discrete: bool = None
         best_feature_idx: int = None
         best_cut_off_feature = None
 
         # 如果可分样本数或者深度满足要求，则继续下一个步骤
-        if n_samples >= self.min_samples_split and depth < self.max_depth:
-            # 双重循环，选择最优属性和最优切分点
-            for feature_idx in range(n_features):
-                feature_array = X[:, feature_idx]
-                unique_feauture_array = np.unique(feature_array)
+        # 双重循环，选择最优属性和最优切分点
+        for feature_idx in range(n_features):
+            feature_array = X[:, feature_idx]
+            is_discrete = is_discrete_target(feature_array)
+            unique_feauture_array = np.unique(feature_array)
 
-                # case1: 所有样本在所有属性上取值相同，无法划分
-                if unique_feauture_array.size == 1:
+            for feature_value in unique_feauture_array:
+                if is_discrete:
+                    mask = (feature_array == feature_value)
+                else:
+                    mask = (feature_array <= feature_value)
+
+                y_of_feature = y[mask]
+                y_out_of_feature = y[np.logical_not(mask)]
+
+                impurity = self._impurity_func(y_of_feature, y_out_of_feature)
+
+                if impurity < self.min_impurity_split:
                     return DecisionTreeNode(leaf_output_value=self._leaf_value_func(y))
 
-                for feature in unique_feauture_array:
-                    mask: np.ndarray = (feature_array == feature)
-                    y_of_feature = y[mask]
-                    y_out_of_feature = y[np.logical_not(mask)]
+                if impurity < min_impurity:
+                    min_impurity = impurity
+                    best_feature_is_discrete = is_discrete
+                    best_feature_idx = feature_idx
+                    best_cut_off_feature = feature_value
 
-                    # case2: 当前结点包含的样本全部属于同一类别
-                    # case3: 当前结点包含的样本集合为空
-                    if len(y_of_feature) == 0 or len(y_out_of_feature) == 0:
-                        return DecisionTreeNode(leaf_output_value=self._leaf_value_func(y))
+        if min_impurity < float("inf"):
+            X_left, X_right, y_left, y_right = self._split_dataset(X, y, best_feature_idx,
+                                                                   best_cut_off_feature, best_feature_is_discrete)
 
-                    impurity = self._impurity_func(y_of_feature, y_out_of_feature)
-
-                    if impurity < min_impurity:
-                        min_impurity = impurity
-                        best_feature_idx = feature_idx
-                        best_cut_off_feature = feature
-
-            # 如果最小的不纯度高于预定阈值，则可以进行特征切分
-            if self.min_impurity_split < min_impurity < float("inf"):
-                X_left, X_right, y_left, y_right = self._split_dataset(X, y, best_feature_idx, best_cut_off_feature)
-                left_child = self._generate_tree(X_left, y_left, depth+1)
-                right_child = self._generate_tree(X_right, y_right, depth+1)
-                root = DecisionTreeNode(feature_idx=best_feature_idx, cut_off_point=best_cut_off_feature,
-                                        left_child=left_child, right_child=right_child)
-                return root
-
-        return DecisionTreeNode(leaf_output_value=self._leaf_value_func(y))
+            left_child = self._generate_tree(X_left, y_left, depth+1)
+            right_child = self._generate_tree(X_right, y_right, depth+1)
+            root = DecisionTreeNode(feature_idx=best_feature_idx, cut_off_point=best_cut_off_feature,
+                                    left_child=left_child, right_child=right_child,
+                                    is_discrete=best_feature_is_discrete)
+            return root
 
     @staticmethod
-    def _split_dataset(X: np.ndarray, y: np.ndarray, feature_idx: int, cut_off_point):
-        n_features = X.shape[1]
-        exclude_mask = np.delete(np.arange(n_features), feature_idx)
-        X_exclude = X.take(exclude_mask, axis=1)
-        mask = X[:, feature_idx] == cut_off_point
-        X_left = X_exclude[mask]
-        X_right = X_exclude[np.logical_not(mask)]
+    def _split_dataset(X: np.ndarray, y: np.ndarray, feature_idx: int, cut_off_point, is_discrete: bool):
+        if is_discrete:
+            mask = (X[:, feature_idx] == cut_off_point)
+        else:
+            mask = (X[:, feature_idx] <= cut_off_point)
+        X_left = X[mask]
+        X_right = X[np.logical_not(mask)]
         y_left = y[mask]
         y_right = y[np.logical_not(mask)]
 
@@ -143,10 +150,13 @@ class DecisionTreeClassifier(BaseDecisionTree):
 
     @staticmethod
     def _gini_index(y: np.ndarray) -> float:
+        if len(y) == 0:
+            return 0.0
+
         y_unique = np.unique(y)
         sum_of_power_of_p = 0.0
         for label in y_unique:
-            p = (label == y).size / y.size
+            p = (label == y).sum() / y.size
             sum_of_power_of_p += math.pow(p, 2)
 
         gini_index = 1 - sum_of_power_of_p
@@ -157,11 +167,21 @@ class DecisionTreeClassifier(BaseDecisionTree):
 
     def _predict_value(self, x: np.ndarray) -> int:
         node: 'DecisionTreeNode' = self.root
-        while node.leaf_output_value is not None:
+
+        while node.leaf_output_value is None:
             feature_value = x[node.feature_idx]
-            if feature_value == node.cut_off_point:
-                node = node.left_child
+            if node.is_discrete:
+                if feature_value == node.cut_off_point:
+                    node = node.left_child
+                else:
+                    node = node.right_child
             else:
-                node = node.right_child
+                if feature_value <= node.cut_off_point:
+                    node = node.left_child
+                else:
+                    node = node.right_child
 
         return node.leaf_output_value
+
+    def score(self, X: np.ndarray, y: np.ndarray) -> float:
+        return accuracy_score(y, self.predict(X))
