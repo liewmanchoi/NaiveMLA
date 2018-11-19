5ede5ec88ca6194cdb6e16cb258f93a804331208
Liew Manchoi <liewmanchoi@gmail.com>
2018-11-19 15:26:35 +0800

    feat: is_discrete_target(array)
    判断数组array是否为离散值数组
    
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ utils/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# __author__ = wangsheng
+# __copyright__ = "Copyright 2018, Trump Organization"
+# __email__ = "liewmanchoi@gmail.com"
+# __status__ = "experiment"
+# __time__ = 2018/11/19 15:19
+# __file__ = __init__.py.py
