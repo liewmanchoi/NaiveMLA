7a2d9c189a4d5b81379f6ecc7714808fbab78b1d
Liew Manchoi <liewmanchoi@gmail.com>
2018-11-26 14:57:19 +0800

    test: 测试文件
    Signed-off-by: Liew Manchoi <liewmanchoi@gmail.com>

---
+++ supervised_learning/tests/test_gradientBoostingClassifier.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# __author__ = wangsheng
+# __copyright__ = "Copyright 2018, Trump Organization"
+# __email__ = "liewmanchoi@gmail.com"
+# __status__ = "experiment"
+# __time__ = 2018/11/26 11:47
+from unittest import TestCase
+import sklearn.ensemble
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_breast_cancer
+from supervised_learning.GradientBoosting import GradientBoostingClassifier
+
+
+# __file__ = test_gradientBoostingClassifier.py
+class TestGradientBoostingClassifier(TestCase):
+    def test(self):
+        cancer = load_breast_cancer()
+        X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, stratify=cancer.target)
+        my_clf = GradientBoostingClassifier(n_estimators=200, learning_rate=0.15)
+        sk_clf = sklearn.ensemble.GradientBoostingClassifier(n_estimators=200)
+        my_clf.fit(X_train, y_train)
+        sk_clf.fit(X_train, y_train)
+        print("Accuracy on training set: {:.3f}".format(my_clf.score(X_train, y_train)))
+        print("Accuracy on test set: {:.3f}".format(my_clf.score(X_test, y_test)))
+        print("Accuracy on training set: {:.3f}".format(sk_clf.score(X_train, y_train)))
+        print("Accuracy on test set: {:.3f}".format(sk_clf.score(X_test, y_test)))
